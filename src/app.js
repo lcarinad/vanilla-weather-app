04c38458289037b76968ee292f40f0add5773fbc
lcarinad <123128869+lcarinad@users.noreply.github.com>
2023-01-25 21:43:33 -0500

    implemented the unit conversion

--- src/app.js
+++ src/app.js
@@ -1,1 +1,96 @@
-alert("Hello World!");
+function searchCity(value) {}
+function formatDate() {
+  let now = new Date();
+  let hours = now.getHours();
+
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  if (hours > 12) {
+    let x = (hours -= 12);
+    return `Last updated: ${day} ${x}:${minutes}pm`;
+  }
+  {
+    return `Last updated: ${day} ${hours}:${minutes}am`;
+  }
+}
+
+function displayTemperature(response) {
+  console.log(response.data.icon);
+  let temperatureElement = document.querySelector("#temperatureElement");
+  let cityElement = document.querySelector("#cityElement");
+  let descriptionElement = document.querySelector("#descriptionElement");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = Math.round(
+    `${response.data.temperature.current}`
+  );
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate();
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.description);
+}
+
+function search(city) {
+  let apiKey = "f5af1dof4ba8f214e4f3d2f50b4bt770";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperatureElement");
+  //remove the active class of the celsius link
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperatureElement");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Pawtucket");
